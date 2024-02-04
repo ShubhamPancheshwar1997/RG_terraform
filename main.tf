384b360664b0d1f4bf7fd30acc4dbdae37b1e053
shubhampancheshwar1997 <shubhampancheshwar@gmai.com>
2024-02-04 12:06:04 +0530

    change into main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "azurem_resource_group" "name" {
   name = "shubhrg1"
   location = "west us"
 }
+resource "azurem_resource_group" "name" {
+  
+  name = "shubhrg2"
+  location = "west us"
+}
